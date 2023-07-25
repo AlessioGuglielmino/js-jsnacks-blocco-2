d9ed4054f4b1796382c72b303f3915db08904d70
AlessioGugielmino <guglielmino.alessio@virgilio.it>
2023-07-25 15:24:41 +0200

    JSnack1:fix

--- JSnack1/main.js
+++ JSnack1/main.js
@@ -6,11 +6,14 @@ let number = prompt("inserisci un numero da 1 a 10 ");
 // scrivo che il numero inserito deve essere un numero intero
 number = parseInt(number);
 
+// se l'user non inserisce un numero mando un alert
+if (isNaN(number)) {
+  alert("inserisci un numero");
+}
 // imposto la condizione, se il numero è paro lo stampo
-if (number % 2 == 0) {
+else if (number % 2 == 0) {
   console.log(number);
 }
-
 // altrimenti se è disparo al numero aggiungo +1
 else {
   console.log(number + 1);
